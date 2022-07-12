de96482e7ed86cc5cd35b146afa8a3357a878b9f
andeledea <62064962+andeledea@users.noreply.github.com>
2022-07-12 16:23:46 +0200

    Prova con subplot fallita

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 from plu import *
 import os
+import tkinter as tk
+from tkinter import filedialog
 
 if __name__ == '__main__':
     # plt.style.use('dark_background')
+    plt.rcParams["figure.autolayout"] = True
+    plt.rcParams["figure.figsize"] = (10, 9)
 
+    h = {}
     folder = 'C:/Elaborazione_profilometro/Symetrics/Txt_files'
-    fname = os.listdir(folder)[5]
-    f = os.path.join(folder, fname)
 
-    if os.path.isfile(f):
-        plu = Plu(f)
+    root = tk.Tk()
+    root.withdraw()
 
-        plu.fitPlane3P()
-        plu.planePlot()
+    files = filedialog.askopenfilenames(parent=root)
 
-        plu.removePlane()
-        plu.pltPlot(fname)
-        plt.show()
+    for fname in files:
+        if os.path.isfile(fname):
+            f = fname.removeprefix(folder)
+            plu = Plu(fname)
+
+            plu.fitPlaneLS_bound(lambda a, b: a < b)
+            # plu.planePlot()
+            plu.removePlane()
+
+            # hist, edges = plu.histMethod(bins=200)
+            # plu.histPlot(hist, edges)
+            # h[f] = plu.findHfromHist(hist, edges)
+
+            plu.pltPlot(f)
+            plu.pltCplot(f)
+            # print(h)
+
+            plt.show()
